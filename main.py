13024f89f179f6759baa3c7d5f8c45a34e7ef2d7
William Stewart <zoidbergwill@gmail.com>
2018-12-24 04:31:56 -0500

    Linting and a bit more error handling

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ def run_bot():
     slack_client = SlackClient(token)
 
     hits = [
-        "guyz",
-        "hey guys",
-        "hi guys",
-        "my guys",
-        "thanks guys",
-        "the guys",
-        "these guys",
-        "those guys",
-        "you guys",
+        'guyz',
+        'hey guys',
+        'hi guys',
+        'my guys',
+        'thanks guys',
+        'the guys',
+        'these guys',
+        'those guys',
+        'you guys',
     ]
 
     base_response = 'Some people in the community find "guys" alienating, next time would you consider _{}_? :slightly_smiling_face: (<http://bit.ly/2uJCn3y|Learn more>)'
@@ -39,7 +39,7 @@ def run_bot():
                 try:
                     events = slack_client.rtm_read()
                 except Exception as exc:
-                    logging.error("Exception during slack_client.rtm_read", exc, events)
+                    logging.error('Exception during slack_client.rtm_read', exc, events)
                     break
                 for event in events:
                     if event.get('type') != 'message':
@@ -49,6 +49,8 @@ def run_bot():
                             continue
                         channel = event['channel']
                         text = event['text']
+                        if 'user' not in event:
+                            continue
                         user = event['user']
                         if any(hit in text.lower() for hit in hits):
                             response = slack_client.api_call(
@@ -59,7 +61,7 @@ def run_bot():
                                 as_user='true',
                             )
                     except Exception as exc:
-                        logging.error("Exception while processing message", exc, event)
+                        logging.error('Exception while processing message', exc, event)
                 time.sleep(1)
         else:
             logging.error('Connection failed, invalid token?')
